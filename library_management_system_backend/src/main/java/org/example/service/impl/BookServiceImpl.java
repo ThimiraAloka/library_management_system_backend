f4137668c4244a174a7349b4464b564ffa6826ae
Thimira Aloka M H <150279156+ThimiraAloka@users.noreply.github.com>
2024-02-02 00:04:59 +0530

    add book method completed

---
+++ library_management_system_backend/src/main/java/org/example/service/impl/BookServiceImpl.java
@@ -0,0 +1,13 @@
+package org.example.service.impl;
+
+import org.example.dto.Book;
+import org.example.service.BookService;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BookServiceImpl implements BookService {
+    @Override
+    public void addBook(Book book) {
+
+    }
+}
